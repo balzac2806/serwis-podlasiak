5e8f172e7400709a2ed896282f83fcf865d0daf2
Daniel Golubiewski <dgolubiewski@9bits.pl>
2017-02-07 12:10:57 +0100

    String To Text Content Dashboard

--- database/migrations/2017_02_06_183415_create_dashboard_table.php
+++ database/migrations/2017_02_06_183415_create_dashboard_table.php
@@ -13,7 +13,7 @@ class CreateDashboardTable extends Migration {
     public function up() {
         Schema::create('dashboard', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('content');
+            $table->text('content');
             $table->timestamps();
         });
     }
