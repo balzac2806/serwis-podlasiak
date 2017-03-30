7708317a5926bfa19b686af20401de111dbf3a2c
Daniel Golubiewski <dgolubiewski@9bits.pl>
2017-03-30 22:36:13 +0200

    Interval

--- app/MoneyReturn.php
+++ app/MoneyReturn.php
@@ -36,7 +36,7 @@ class MoneyReturn extends Authenticatable {
     
      public function getTimeAttribute($value) {
         if (!empty($value)) {
-            $value = date('Y-m-d H:i:s', strtotime('+1 hours',strtotime($value)));
+            $value = date('Y-m-d H:i:s', strtotime('+1 hour',strtotime($value)));
         } 
         return $value;
     }
